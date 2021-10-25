44507f7ad6b72c6c4f0db1ad76f55c0c7321666b
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-10-25 20:56:24 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function calcularAreaTriangulo(base: number, altura: number) {
+  let area: number = (base * altura) / 2;
+  console.log("el area es " + area);
+}
+
+calcularAreaTriangulo(1, 2);
+calcularAreaTriangulo(2, 4);
+calcularAreaTriangulo(3, 6);
+calcularAreaTriangulo(4, 8);
+calcularAreaTriangulo(5, 10);
+calcularAreaTriangulo(6, 12);
+calcularAreaTriangulo(7, 14);
